b953c3835082e06197183d292dde9868c02673cd
DylanMacres <Dylan.Macres@du.edu>
2022-09-28 13:52:13 -0600

    added better css and html to make the website look better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/Manager");
-// const indexfile = 
+// const indexfile =
 //adding the dependicies //
 
 const inquirer = require("inquirer");
@@ -14,222 +14,221 @@ const jest = require("jest");
 const { resolve } = require("path");
 const { start } = require("repl");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath =path.join(OUTPUT_DIR, "profile.html")
-
-// Array  for the team to be pushed to 
-let employees = [];
+const outputPath = path.join(OUTPUT_DIR, "profile.html");
 
 
-  let questionsManager = ([
-    {
-      type: "input",
-      message: "What is your manager's name?",
-      name:'name',
-    },
-    {
-      type: "input",
-      message: "What is your manager's id number",
-      name: "ID",
-    },
-    {
-      type: "input",
-      message: "What is your manager's email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is the manager's office number?",
-      name: "officeNumber",
-    },
-  ])
-  
-  function managerHtml (){
-    inquirer.prompt(questionsManager)
-  .then(response => {
-    const manager = new Manager (response.name, response.ID, response.email, response.officeNumber)
-    employees.push(manager);
-    // const startPage = fs.writeFile("./dist/final.html", )
 
+let questionsManager = [
+  {
+    type: "input",
+    message: "What is your manager's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your manager's id number",
+    name: "ID",
+  },
+  {
+    type: "input",
+    message: "What is your manager's email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the manager's office number?",
+    name: "officeNumber",
+  },
+];
 
-  const managerCard = `<!DOCTYPE html>
+function managerHtml() {
+  inquirer.prompt(questionsManager).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.ID,
+      response.email,
+      response.officeNumber
+    );
+   
+
+    const managerCard = `<!DOCTYPE html>
   <html lang="en">
   <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="dist/css/style.css"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
       <title>Team Profile Generator</title>
   </head>
   <body>
-  
+  <div>
       <header class="text-center custom header">
           <h3>Meet the Team!</h1>
-      
       </header>
-  
-      <div class="container-fluid"></div>
-      <section class="row"></section>
-  
-  <form>
-  <div class= "col-sm-3">
-    
+      <div class="container"></div>
+      <div class="row"></div>
+  <div>
   </div>
-  </form>
-  <div class="col-12 col-md-10">>
   </div>
+  <section class="row">
   <div class=" text-center">
-      <h3>hello world!</h3></div>
-      
-      
-      <section class="container-fluid  ">
-          <div class="row">
-            <div class="col-2 boxes card">
-              <h4 class="card-header" id="day1">${manager.getName()}</h4>
+          <div class="card" style="width: 20rem;">
+            <div class="card-body">
+              <h4 class="card-header text-center">${manager.getName()}</h4>
               <h5>${manager.getRole()}</h5>
             </div>
-            <div class="cardInfo">
-            <p> ID: ${manager.getID()}</p>
-            <p> Email:<a href="Email to:" ${manager.getEmail()}"${manager.getEmail()}</p></a>
-            <p> Office Number: ${manager.getOfficeNum()}</p>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item"> ID: ${manager.getID()}</li>
+            <li class="list-group-item"> <a href="Emailto: ${
+              manager.email
+            }" class="card-link">${manager.email}</a></li>
+            <li class="list-group-item"> Office Number: ${manager.getOfficeNum()}</li>
+            </ul>
             </div>
-            </section>
          </div>   
             `;
-            fs.writeFile("./dist/final.html", managerCard,function(err){
-              if(err){
-                console.log('not working')
-              }
-            } )
-            // fs.writeFile("./dist/final.html", managerCard, (err)=> 
-            // err ? console.error(err): console.log('IT WORKED!'))
-            whatsNext();
-  })
-}
-
-  let internQuestions = ([
-    {
-      type: "input",
-      message: "What is your intern's name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your intern's id number",
-      name: "ID",
-    },
-    {
-      type: "input",
-      message: "What is your intern's email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What school is/did your intern attend?",
-      name: "school",
-    },
-  ])
-  function empIntern() {
-    inquirer.prompt(internQuestions)
-  .then (response => {
-    const intern = new Intern (response.name, response.ID, response.email, response.school );
-    employees.push(intern);
-    const internCard = `<div class="col-2 boxes card">
-    <h4 class="card-header" id="day1">${intern.getName()}</h4>
-    <h5>${intern.getRole()}</h5>
-  </div>
-  <div class="cardInfo">
-  <p> ID: ${intern.getID()}</p>
-  <p> Email:<a href="Email to:" ${intern.getEmail()}"${intern.getEmail()}</p></a>
-  <p> Office Number: ${intern.getSchool()}</p>
-  </div>`
-  fs.writeFile("./dist/final.html", internCard,function(err){
-    if(err){
-      console.log('not working')
-    }
-  } )
+    fs.writeFile("./dist/final.html", managerCard, function (err) {
+      if (err) {
+        console.log("not working");
+      }
+    });
+    // fs.writeFile("./dist/final.html", managerCard, (err)=>
+    // err ? console.error(err): console.log('IT WORKED!'))
     whatsNext();
- 
-  })
+  });
 }
-  let engineerQuestions = ([
-  
-    {
-    type: 'input',
-    message: "What is your Engineer'0s name?",
-    name:'name'
 
+let internQuestions = [
+  {
+    type: "input",
+    message: "What is your intern's name?",
+    name: "name",
   },
-    {
-      type: 'input',
-      message: "What is the engineer's id?",
-      name:'ID'
-    },
-
-    {
-      name: 'input',
-      message: "What is the engineer's email?",
-      name: 'email'
-
-    },
-
-    {
-      type: 'input',
-      message: "What is your engineer's github username?",
-      name:'github'
-
-    }
-
-  ])
-  function empEngineer(){
-    inquirer.prompt(engineerQuestions)
-  .then(response => {
-    const engineer = new Engineer(response.name, response.ID, response.email, response.github);
-    employees.push(engineer);
-    const engineerCard = `<div class="col-2 boxes card">
-    <h4 class="card-header" id="day1">${engineer.getName()}</h4>
-    <h5>${engineer.getRole()}</h5>
-  </div>
-  <div class="cardInfo">
-  <p> ID: ${engineer.getID()}</p>
-  <p> Email:<a href="Email to:" ${engineer.getEmail()}"${engineer.getEmail()}</p></a>
-  <p> Office Number: ${engineer.getGithub()}</p>
-  </div>`
-  fs.writeFile("./dist/final.html", engineerCard,function(err){
-    if(err){
-      console.log('not working')
-    }
-  } )
+  {
+    type: "input",
+    message: "What is your intern's id number",
+    name: "ID",
+  },
+  {
+    type: "input",
+    message: "What is your intern's email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What school is/did your intern attend?",
+    name: "school",
+  },
+];
+function empIntern() {
+  inquirer.prompt(internQuestions).then((response) => {
+    const intern = new Intern(
+      response.name,
+      response.ID,
+      response.email,
+      response.school
+    );
+    employees.push(intern);
+    const internCard = `<div class="card" style="width: 20rem;">
+    <div class="card-body">
+      <h4 class="card-header text-center">${intern.getName()}</h4>
+      <h5>${intern.getRole()}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item"> ID: ${intern.getID()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}" class="card-link">${intern.getEmail()}</a></li>
+    <li class="list-group-item"> School: ${intern.getSchool()}</li>
+    </ul>
+    </div>
+ </div>   `;
+    fs.appendFile("./dist/final.html", internCard, function (err) {
+      if (err) {
+        console.log("not working");
+      }
+    });
     whatsNext();
-  })
-};
+  });
+}
+let engineerQuestions = [
+  {
+    type: "input",
+    message: "What is your Engineer's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's id?",
+    name: "ID",
+  },
 
+  {
+    name: "input",
+    message: "What is the engineer's email?",
+    name: "email",
+  },
 
+  {
+    type: "input",
+    message: "What is your engineer's github username?",
+    name: "github",
+  },
+];
+function empEngineer() {
+  inquirer.prompt(engineerQuestions).then((response) => {
+    const engineer = new Engineer(
+      response.name,
+      response.ID,
+      response.email,
+      response.github
+    );
+    employees.push(engineer);
+    const engineerCard = `<div class="card" style="width: 20rem;">
+    <div class="card-body">
+      <h4 class="card-header text-center">${engineer.getName()}</h4>
+      <h5>${engineer.getRole()}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item"> ID: ${engineer.getID()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a></li>
+    <li class="list-group-item"> GitHub: ${engineer.getGithub()}</li>
+    </ul>
+    </div>
+ </div> `;
+    fs.appendFile("./dist/final.html", engineerCard, function (err) {
+      if (err) {
+        console.log("not working");
+      }
+    });
+    whatsNext();
+  });
+}
 
-    function whatsNext() {
-      inquirer
-        .prompt([
-          {
-            type: 'list',
-            message:'What would you like to do next?',
-            choices:["Add Engineer", "Add Intern", "Finish"],
-            name:'choicesList',
-          }
-        ]).then((response) => {
-          if(response.choicesList == "Add Engineer"){
-            empEngineer();
-    } else if(response.choicesList == "Add Intern"){
-      empIntern();
-    
-    }else {
-      noMore();
-    }
-    
-        })
-    }
+function whatsNext() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do next?",
+        choices: ["Add Engineer", "Add Intern", "Finish"],
+        name: "choicesList",
+      },
+    ])
+    .then((response) => {
+      if (response.choicesList == "Add Engineer") {
+        empEngineer();
+      } else if (response.choicesList == "Add Intern") {
+        empIntern();
+      } else {
+        noMore();
+      }
+    });
+}
 
 managerHtml();
 // empIntern();
 // empEngineer();
-function noMore(){
+function noMore() {
   return;
 }
